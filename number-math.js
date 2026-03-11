d8f5b2d3dceb685ebae5a6d309074ed960b29306
filipe satiro <filipelipe202229@gmail.com>
2026-03-11 03:05:38 -0300

    Estudando o Math, abs,ceil,floor,round e random em JS

---
+++ number-math.js
@@ -0,0 +1,25 @@
+//console.log(parseFloat('99.70')); //antiga
+//console.log(Number.parseFloat('300.50')); //nova
+//console.log(parseInt(258.74));
+// const v = 5.90;
+// const vString = v.toString();
+// console.log(v.toString());
+// const valor = 5.88;
+// const moeda = valor.toLocaleString('en-US', { style: 'currency',currency: 'USD'});
+// const cadastro = {
+//     style: 'currency',
+//     currency: 'USD'
+// }
+// console.log(moeda);
+//console.log(Math.abs(-1)); //sempre positivo abs
+//console.log(Math.ceil(4.1)); //arredonda sempre paara cima
+//console.log(Math.floor(4.1)); //arredonda sempre paara baixo
+//console.log(Math.round(2.59)); //arredonda  paara cima ou  baixo
+
+// const aleatorio1 = Math.random() *10;
+// const arredondar1 = Math.floor(aleatorio1);
+
+// console.log(arredondar1);
+
+//Math.floor(Math.random() * (50 - 40 + 1)) + 40;
+console.log(Math.floor(Math.random() * (50 - 40 + 1)) + 40);
