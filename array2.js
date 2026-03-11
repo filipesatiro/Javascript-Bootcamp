4c491a622d4221c9ee07749a3a78e71f6a96dfff
filipe satiro <filipelipe202229@gmail.com>
2026-03-11 08:52:17 -0300

    Criado o arquivo array2 para aprender sobre copyWithin

---
+++ array2.js
@@ -0,0 +1,11 @@
+//copyWithin (0,0, array.length)
+//copyWithin(alvo, inicioCopia, pausaCopia);
+//index 0,1,2,3,4,5,6,7,8,9
+// const n =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] //posição da array
+//console.log(n.copyWithin(2)); 
+//console.log(n.copyWithin(5, 8));
+//console.log(n.copyWithin(2, 6, 7));
+
+const pessoas = ['Arthur', 'Enzo', 'Zoi','Sophia', 'Alan'];
+
+console.log(pessoas.copyWithin(2, 3, 3));
