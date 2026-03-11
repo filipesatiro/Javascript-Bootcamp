68b1e9453c921cce86063bd2f3646bda7aca22c7
filipe satiro <filipelipe202229@gmail.com>
2026-03-11 00:36:53 -0300

    Criado o arquivo para estudar indexof, padstart e repeat

---
+++ indexof-lastindexof-padstart-padend-repeat.js
@@ -0,0 +1,12 @@
+
+
+
+// const valor = ['R$ 40', 'R$ 500', 'R$ 1700', 'R$ 3500', 'R$ 10'];
+
+
+// valor.forEach((i)=> {
+//     console.log(i.padStart(10, '-'))
+// })
+const palavra = 'like ';
+
+console.log(spanLike.innerHTML = palavra.repeat(1000));
