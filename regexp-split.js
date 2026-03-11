bfe06afd94efc6d27d2125ba25e6a0bf238a640d
filipe satiro <filipelipe202229@gmail.com>
2026-03-11 02:13:04 -0300

    Estudando split e regexp e seus metodos

---
+++ regexp-split.js
@@ -0,0 +1,9 @@
+let cardapio = 'xsaladas xeegs xfrangos xbacons xtudos';
+
+// cardapio = 'oi';
+//cardapio = cardapio.replace('xsalada', 'dogcompleto');
+//cardapio = cardapio.replace(' ', ',')
+//cardapio = cardapio.replace(/[ ]+/g, ',');
+ 
+const cardapioArray = cardapio.split('s ');
+console.log(cardapioArray);
