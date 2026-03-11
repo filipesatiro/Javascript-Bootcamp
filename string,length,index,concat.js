b6bee68c381210d314e9489b204e733652b65737
filipe satiro <filipelipe202229@gmail.com>
2026-03-11 00:03:37 -0300

    Aprendendo a contar letras e comcatenar com JS

---
+++ string,length,index,concat.js
@@ -0,0 +1,8 @@
+
+
+const frase1 = 'Viajar é bom demais ';
+const frase2 = 'principalmente com saúde ';
+const frase3 = 'e muito dinheiro no bolso!';
+
+const fraseCompleta = frase1.concat(frase2,frase3)
+console.log(fraseCompleta);
